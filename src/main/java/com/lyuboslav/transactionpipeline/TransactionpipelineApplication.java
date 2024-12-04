57ac1adb223bedc5ca51e955f34e17bc739b7cb0
Lyuboslav Marinov <lmarinov@experlogix.com>
2024-12-04 23:20:18 +0200

    First commit

---
+++ src/main/java/com/lyuboslav/transactionpipeline/TransactionpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.lyuboslav.transactionpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionpipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lyuboslav/transactionpipeline/TransactionpipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyuboslav.transactionpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionpipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
